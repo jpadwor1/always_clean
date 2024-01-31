81bc19b35c406fc266f35b49be579cdff9bd6f9d
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-01-31 08:58:33 -0800

    styles: update feature icons for tile deck cleaning page

--- src/app/services/tile-deck-cleaning/page.tsx
+++ src/app/services/tile-deck-cleaning/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FlaskConical, Pipette, TestTubes } from 'lucide-react';
+import { FlaskConical, Leaf, Paintbrush, Pipette, SprayCan, TestTubes } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 const Page = () => {
@@ -55,7 +55,7 @@ const Page = () => {
             <div className='w-full md:w-1/2 lg:w-1/3 px-4'>
               <div className='h-full p-8 text-center hover:bg-white rounded-md hover:shadow-xl transition duration-200'>
                 <div className='inline-flex h-16 w-16 mb-7 mx-auto items-center justify-center text-white bg-blue-500 rounded-lg'>
-                  <FlaskConical className='h-8 w-8' />
+                  <SprayCan className='h-8 w-8' />
                 </div>
                 <h3 className='mb-4 text-xl md:text-2xl leading-tight font-bold'>
                   Deep Cleaning for Tiles
@@ -69,7 +69,8 @@ const Page = () => {
             <div className='w-full md:w-1/2 lg:w-1/3 px-4'>
               <div className='h-full p-8 text-center hover:bg-white rounded-md hover:shadow-xl transition duration-200'>
                 <div className='inline-flex h-16 w-16 mb-7 mx-auto items-center justify-center text-white bg-blue-500 rounded-lg'>
-                  <TestTubes className='h-8 w-8' />
+                  <Paintbrush className='h-8 w-8' />
+                  
                 </div>
                 <h3 className='mb-4 text-xl md:text-2xl leading-tight font-bold'>
                   Deck Restoration
@@ -83,7 +84,7 @@ const Page = () => {
             <div className='w-full md:w-1/2 lg:w-1/3 px-4'>
               <div className='h-full p-8 text-center hover:bg-white rounded-md hover:shadow-xl transition duration-200'>
                 <div className='inline-flex h-16 w-16 mb-7 mx-auto items-center justify-center text-white bg-blue-500 rounded-lg'>
-                  <Pipette className='h-8 w-8' />
+                  <Leaf className='h-8 w-8' />
                 </div>
                 <h3 className='mb-4 text-xl md:text-2xl leading-tight font-bold'>
                   Safe & Eco-Friendly Solutions
