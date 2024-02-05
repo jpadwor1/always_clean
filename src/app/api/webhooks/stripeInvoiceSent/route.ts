d2669dc6875deac49d8364a86de1a2489005efe5
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-02-04 16:51:35 -0800

    refactor: add additional error handling instripeInvoice endpoint

--- src/app/api/webhooks/stripeInvoiceSent/route.ts
+++ src/app/api/webhooks/stripeInvoiceSent/route.ts
@@ -5,38 +5,40 @@ import type Stripe from 'stripe';
 
 export async function POST(request: Request) {
   const body = await request.text();
-  const signature = headers().get('stripe-signature') ?? '';
-
-  let event: Stripe.Event;
+  const signature = request.headers.get('stripe-signature') || '';
 
   try {
-    event = stripe.webhooks.constructEvent(
+    const event = stripe.webhooks.constructEvent(
       body,
       signature,
       process.env.INVOICE_SENT_WEBHOOK_SECRET || ''
     );
+
+    if (event.type === 'invoice.sent') {
+      const customer = await stripe.customers.retrieve(
+        event.data.object.customer as string
+      );
+
+      await db.customer.update({
+        where: {
+          stripeSubscriptionId: customer.id,
+        },
+        data: {
+          stripeBalanceDue: true,
+        },
+      });
+
+      return new Response(null, { status: 200 });
+    } else {
+      return new Response('Webhook event not handled', { status: 400 });
+    }
   } catch (err) {
-    return new Response(
-      `Webhook Error: ${err instanceof Error ? err.message : 'Unknown Error'}`,
-      { status: 400 }
-    );
+    console.error('Webhook Error:', err);
+
+    if (err instanceof stripe.errors.StripeSignatureVerificationError) {
+      return new Response('Invalid Stripe signature', { status: 400 });
+    } else {
+      return new Response('Internal server error', { status: 500 });
+    }
   }
-
-  if (event.type === 'invoice.sent') {
-    // Retrieve the details from Stripe.
-    const customer = await stripe.customers.retrieve(
-      event.data.object.customer as string
-    );
-
-    await db.customer.update({
-      where: {
-        stripeSubscriptionId: customer.id,
-      },
-      data: {
-        stripeBalanceDue: true,
-      },
-    });
-  }
-
-  return new Response(null, { status: 200 });
 }
