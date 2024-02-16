a5dce7392bcac154986db34513000b78af06eeb1
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-02-15 20:36:09 -0800

    refactor: update available times in booking form

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -123,13 +123,6 @@ const BookingForm: React.FC<BookingFormProps> = ({ activeStep }) => {
     form.register('address').ref
   );
   const availableTimes = [
-    '9:00 AM',
-    '10:00 AM',
-    '11:00 AM',
-    '12:00 PM',
-    '1:00 PM',
-    '2:00 PM',
-    '3:00 PM',
     '4:00 PM',
     '5:00 PM',
   ];
