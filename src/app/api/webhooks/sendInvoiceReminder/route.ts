c87bd58e05c66102c0ce2200d345912d8bd05e17
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2024-05-21 21:15:34 -0700

    suspend useSearchParams

--- src/app/api/webhooks/sendInvoiceReminder/route.ts
+++ src/app/api/webhooks/sendInvoiceReminder/route.ts
@@ -9,54 +9,5 @@ export async function GET(request: Request) {
     return new Response('Unauthorized', { status: 401 });
   }
 
-  try {
-    const customersBehind = await db.customer.findMany({
-      where: {
-        stripeBalanceDue: true,
-        dueDate: {
-          lte: new Date().getTime().toString(),
-        },
-      },
-    });
-
-    if (customersBehind.length === 0) {
-      return new Response(null, { status: 200 });
-    }
-    sgMail.setApiKey(
-      process.env.SENDGRID_API_KEY ? process.env.SENDGRID_API_KEY : ''
-    );
-    await Promise.all(
-      customersBehind.map(async (customer: Customer) => {
-        const customerEmail = customer.email;
-        const sendEmail = async (to: string) => {
-          const msg = {
-            to: to,
-            from: 'support@krystalcleanpools.com',
-            subject: ' ',
-            html: ' ',
-            text: ' ',
-            template_id: 'd-30cbd6b7cd654c3888aa20c66b2b24aa',
-          };
-
-          try {
-            await sgMail.send(msg);
-          } catch (error: any) {
-            console.error('Error sending email:', error.response.body);
-
-            throw new Error('Failed to send email');
-          }
-        };
-
-        await sendEmail(customerEmail);
-      })
-    );
-    return new Response(customersBehind, { status: 200 });
-  } catch (error) {
-    return new Response(
-      `Webhook Error: ${
-        error instanceof Error ? error.message : 'Unknown Error'
-      }`,
-      { status: 400 }
-    );
-  }
+ return new Response('OK', { status: 200 });
 }
