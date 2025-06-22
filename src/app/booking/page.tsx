0cd7e4caa60b4d68af7398c82ab1d4276483c763
John Padworski <128070765+jpadwor1@users.noreply.github.com>
2025-06-21 21:27:55 -0700

    replaced /booking form with calendly embed

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -4,22 +4,28 @@ import React from 'react';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import { Separator } from '@/components/ui/separator';
 import StepperForm from '@/components/Stepper';
+import CalendlyEmbed from '@/components/booking/CalendlyEmbed';
 
 const Page = () => {
-  return (
-    <MaxWidthWrapper className='mt-10'>
-      <div className='flex flex-col justify-between items-start mb-5 bg-white shadow-md rounded-md'>
-        <div className='flex flex-col px-4 py-4'>
-          <h1 className='text-3xl font-bold'>Booking</h1>
-          <p className='mt-4 text-md text-slate-500 '>
-            {' '}
-            Check our availability and schedule your service
-          </p>
+  return (<>
+    <MaxWidthWrapper className='mt-10 h-100 w-full py-0 '>
+      <div className='flex flex-col justify-between bg-white rounded-md shadow-md'>
+
+        <div className='flex flex-col justify-between items-start bg-white '>
+          <div className='flex flex-col px-4 py-4'>
+            <h1 className='text-3xl font-bold'>Booking</h1>
+            <p className='mt-4 text-md text-slate-500 '>
+              {' '}
+              Check our availability and schedule your service
+            </p>
+          </div>
+
         </div>
-        <Separator className='my-4 mx-2 w-[95%]' />
-        <StepperForm />
+        <CalendlyEmbed url="https://calendly.com/krystalcleanpools-support/30min" />
       </div>
+
     </MaxWidthWrapper>
+  </>
   );
 };
 
