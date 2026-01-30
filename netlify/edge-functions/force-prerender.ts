be7c006bd2e50178089837b20c9dfc5118a48a34
John Padworski <128070765+jpadwor1@users.noreply.github.com>
2026-01-29 20:44:24 -0800

    add redirect and header files

--- netlify/edge-functions/force-prerender.ts
+++ netlify/edge-functions/force-prerender.ts
@@ -1,14 +1,20 @@
 export default async (request: Request) => {
   const ua = request.headers.get("user-agent") || "";
 
+  // Prevent infinite loop
+  if (request.headers.get("x-prerender-checked") === "true") {
+    return fetch(request);
+  }
+
   if (
     ua.includes("AdsBot-Google") ||
     ua.includes("Google-Ads-Creatives-Assistant")
   ) {
     const headers = new Headers(request.headers);
     headers.set("X-Prerender-Force", "true");
+    headers.set("x-prerender-checked", "true");
 
-    return fetch(request, { headers });
+    return fetch(new Request(request, { headers }));
   }
 
   return fetch(request);
